39baeaf8f5544a213d62f6c0ad0a2ecef495b038
Johannes Rabausch <jrabausch@absatzformat.de>
2021-06-13 16:58:34 +0200

    Added request generator and exception handling

--- src/RequestRunner.php
+++ src/RequestRunner.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 
 namespace Semperton\Porter;
 
+use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use RuntimeException;
 use Semperton\Porter\Emitter\EmitterInterface;
+use Throwable;
 
 final class RequestRunner
 {
@@ -16,16 +19,42 @@ final class RequestRunner
 	/** @var EmitterInterface */
 	protected $responseEmitter;
 
-	public function __construct(RequestHandlerInterface $requestHandler, EmitterInterface $responseEmitter)
-	{
+	/** @var callable */
+	protected $serverRequestGenerator;
+
+	/** @var callable|null */
+	protected $errorHandler;
+
+	public function __construct(
+		RequestHandlerInterface $requestHandler,
+		EmitterInterface $responseEmitter,
+		callable $serverRequestGenerator,
+		?callable $errorHandler = null
+	) {
 		$this->requestHandler = $requestHandler;
 		$this->responseEmitter = $responseEmitter;
+		$this->serverRequestGenerator = $serverRequestGenerator;
+		$this->errorHandler = $errorHandler;
 	}
 
-	public function run(ServerRequestInterface $request): void
+	public function run(): void
 	{
-		$response = $this->requestHandler->handle($request);
+		try {
+			/** @var ServerRequestInterface */
+			$request = ($this->serverRequestGenerator)();
+			$response = $this->requestHandler->handle($request);
 
-		$this->responseEmitter->emit($response);
+			$this->responseEmitter->emit($response);
+		} catch (Throwable $exception) {
+
+			if ($this->errorHandler === null) {
+				throw new RuntimeException('Unable to generate response for exception', 0, $exception);
+			}
+
+			/** @var ResponseInterface */
+			$response = ($this->errorHandler)($exception);
+
+			$this->responseEmitter->emit($response);
+		}
 	}
 }
